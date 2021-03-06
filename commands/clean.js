1f2e51a0b27fc7363d9e18edcb70ae7aa37e6c15
amaury forest <amaury.forest@etu.univ-lyon1.fr>
2021-03-07 00:17:39 +0100

    Feat(clean): Add --force param to delete all messages

--- commands/clean.js
+++ commands/clean.js
@@ -4,28 +4,34 @@ const config = require('../config.json');
 module.exports = {
 	name: "clean",
 	desc: "Clear all bot and user command messages",
-	execute: function(message) {
+	execute: function(message, args) {
 		if (message.member.hasPermission("MANAGE_MESSAGES")) {
 			message.channel.messages.fetch()
 				.then(function(list){
-					// message.channel.bulkDelete(list);
 					let msgToDelete = [];
-					list.forEach((value, key, map) => {
-						if(value.author.id == message.client.user.id){
-							msgToDelete.push(list.get(key));
-						}
-						else if(value.content.charAt(0) == config.PREFIX){
-							msgToDelete.push(list.get(key));
-						}
-					})
-					message.channel.bulkDelete(msgToDelete);
+					if(args == "--force"){
+						// TODO : Limiter l'accès à cette fonction
+						msgToDelete = list;
+					}
+					else{
+						list.forEach((value, key, map) => {
+							if(value.author.id == message.client.user.id){
+								msgToDelete.push(list.get(key));
+							}
+							else if(value.content.charAt(0) == config.PREFIX){
+								msgToDelete.push(list.get(key));
+							}
+						})
+					}
+					message.channel.bulkDelete(msgToDelete, true);
 					if(msgToDelete.length > 0){
 						// La fonction ne peut delete que 50 messages à la fois
 						// La relancer si on a supprimer des messages
 						// Si on a rien supprimé, sert à rien de relancer, c'est qu'on a déjà tout clean
 						this.execute(message);
 					}
-				}, function(err){
+				}.bind(this),
+				function(err){
 					message.channel.send("ERROR: ERROR CLEARING CHANNEL.")
 				})
 		}
--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,45 @@
 const config = require('../config.json');
+const Discord = require('discord.js');
 const prefix = config.PREFIX;
 
 module.exports = {
 	name: "help",
 	desc : "Gives you the list of all command and prefix",
 	execute: function(message) {
-		let helpTxt = "Prefix command is "+prefix+"\nCommand list is\n";
+		let description = "Prefix command is "+prefix;
 
-		message.client.commands.forEach((value, key, map)=> {
-			helpTxt += value.name + " : " + value.desc + "\n";
-		})
+		const exampleEmbed = {
+			color: 0x0099ff,
+			title: 'SuperRedBot',
+			description: description,
+			url: 'https://github.com/Redblu',
+			fields: craftContent(message),
+			timestamp: new Date(),
+			footer: {
+				text: 'Redblu.',
+				icon_url: 'https://i.imgur.com/wSTFkRM.png',
+			},
+		};
 
-		message.author.send(helpTxt);
+		message.author.send({ embed: exampleEmbed });
 	}
+}
+
+function craftContent(message){
+	let fields = [
+		{
+			name: 'Current bot status',
+			value: 'Still in development ',
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		}
+	]
+	message.client.commands.forEach((value, key, map)=> {
+		fields.push({name: value.name, value: value.desc});
+	});
+
+	return fields;
 }
