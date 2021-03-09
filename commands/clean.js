ce8ecfef122bfcead0da20544bc9e838d7986e2b
amaury forest <amaury.forest@etu.univ-lyon1.fr>
2021-03-09 14:51:46 +0100

    Feat(clean): Restrict use of --force to admins set in config file + Feat(clean): clean force only the last x messages

--- commands/clean.js
+++ commands/clean.js
@@ -9,10 +9,27 @@ module.exports = {
 			message.channel.messages.fetch()
 				.then(function(list){
 					let msgToDelete = [];
-					if(args == "--force"){
-						// TODO : Limiter l'accès à cette fonction
-						msgToDelete = list;
+
+					if(args != null && args.length >0 && config.ADMINS.includes(message.author.id)){
+						if(args.length === 1 && args[0] === "--force"){
+							msgToDelete = list;
+						}
+						else if (args.length === 2 && args[0] === "--force" && !isNaN(Number(args[1]))){
+							let numberToDelete = Number(args[1])+1;
+							let cpt = 0;
+							list.forEach((value, key, map) => {
+								if(cpt < numberToDelete){
+									msgToDelete.push(list.get(key));
+								}
+								cpt ++;
+							});
+
+							if(numberToDelete > 50){
+								args[1] = numberToDelete - 50; // Will recall this function to delete the others 50+ msg
+							}
+						}
 					}
+
 					else{
 						list.forEach((value, key, map) => {
 							if(value.author.id == message.client.user.id){
@@ -23,12 +40,14 @@ module.exports = {
 							}
 						})
 					}
+
 					message.channel.bulkDelete(msgToDelete, true);
-					if(msgToDelete.length > 0){
+
+					if((msgToDelete.length > 0 && args == null) || (args != null && args.length > 1 && Number(args[1]) > 0)){
 						// La fonction ne peut delete que 50 messages à la fois
 						// La relancer si on a supprimer des messages
 						// Si on a rien supprimé, sert à rien de relancer, c'est qu'on a déjà tout clean
-						this.execute(message);
+						this.execute(message, args);
 					}
 				}.bind(this),
 				function(err){
